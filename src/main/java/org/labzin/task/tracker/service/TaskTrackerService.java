81f5ae623c777d4b555dd7a61d3e26fab7285055
Ivan Labzin <i.labzin@mail.astondevs.ru>
2026-01-23 22:28:26 +0300

    Exception and Create

--- src/main/java/org/labzin/task/tracker/service/TaskTrackerService.java
+++ src/main/java/org/labzin/task/tracker/service/TaskTrackerService.java
@@ -3,6 +3,8 @@ package org.labzin.task.tracker.service;
 import lombok.AllArgsConstructor;
 import org.labzin.task.tracker.dto.ProjectDto;
 import org.labzin.task.tracker.exceptions.BadRequestException;
+import org.labzin.task.tracker.exceptions.NotFoundException;
+import org.labzin.task.tracker.factories.ProjectDtoFactory;
 import org.labzin.task.tracker.model.ProjectEntity;
 import org.labzin.task.tracker.repository.ProjectRepository;
 import org.springframework.stereotype.Service;
@@ -13,18 +15,35 @@ import org.springframework.transaction.annotation.Transactional;
 public class TaskTrackerService {
 
     private final ProjectRepository projectRepository;
+    private final ProjectDtoFactory projectDtoFactory;
 
     @Transactional
     public ProjectDto createProject(String name) {
 
         if(projectRepository.findByName(name).isPresent()) {
-            throw new BadRequestException(String.format("Проект с названием \"%s\" уже есть", name));
+            throw new BadRequestException(String.format("Project \"%s\" already exist", name));
         }
 
-        throw new BadRequestException(String.format("Project \"%s\" already exist", name));
+        ProjectEntity project = ProjectEntity.builder().name(name).build();
+        projectRepository.save(project);
 
-        //projectRepository.save(ProjectEntity.builder().name(name).build());
+    return projectDtoFactory.makeProjectDto(project);
+    }
 
-    //return null;
+    public ProjectDto editProject(Long projectId, String name) {
+
+        if(name.trim().isEmpty()) {
+            throw new BadRequestException(String.format("\"%s\" can not be empty", name));
+        }
+
+        ProjectEntity project = projectRepository.findById(projectId)
+                .orElseThrow(() -> new NotFoundException(String.format("Project \"%s\" not found", name)));
+
+        projectRepository.findByName(name).filter(anotherProject -> !anotherProject.getId().equals(projectId))
+                .ifPresent(anotherProject -> {
+                    throw new BadRequestException(String.format("\"%s\" duplicate", name));
+                });
+
+        return projectDtoFactory.makeProjectDto(project);
     }
 }
